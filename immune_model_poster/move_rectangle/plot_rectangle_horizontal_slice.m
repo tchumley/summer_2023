0c04c92938f2ad5d99f33ab7bafe010ca870b2d2
Cathy <135647946+Cathy-Yicheng-Liu@users.noreply.github.com>
2023-07-26 11:26:05 -0400

    Update plot_rectangle_horizontal_slice.m

--- immune_model_poster/move_rectangle/plot_rectangle_horizontal_slice.m
+++ immune_model_poster/move_rectangle/plot_rectangle_horizontal_slice.m
@@ -3,7 +3,9 @@ kvalue = [1000:1000:11000];
 tvalue = [4:1:12];
 [t,k] = meshgrid(tvalue,kvalue);
 
-
+% create nine probability_matrix that record probability of excursion based on kick sizes for each tau value
+% plot each probability_matrix to get a slice
+% hold on to get the whole graph
 for i = 1:11
     A=zeros(1,2)+kvalue(i);
     probability_matrix = zeros(9, 2);
