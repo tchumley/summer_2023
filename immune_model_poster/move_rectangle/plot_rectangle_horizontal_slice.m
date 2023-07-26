7f8596339365732870178ccb3efd8d35b195fd61
Cathy <135647946+Cathy-Yicheng-Liu@users.noreply.github.com>
2023-07-26 10:47:51 -0400

    Add files via upload

---
+++ immune_model_poster/move_rectangle/plot_rectangle_horizontal_slice.m
@@ -0,0 +1,28 @@
+probabilities = importdata("probability_of_excursion_data_matrix.mat");
+kvalue = [1000:1000:11000];
+tvalue = [4:1:12];
+[t,k] = meshgrid(tvalue,kvalue);
+
+
+for i = 1:11
+    A=zeros(1,2)+kvalue(i);
+    probability_matrix = zeros(9, 2);
+    probability_matrix(:,2) = probabilities(i,:);
+    surf(A,tvalue,probability_matrix,'EdgeColor','white')
+    hold on
+end
+
+xlabel('kick size')
+ylabel('mean flow time')
+zlabel('probability of excursion')
+
+grid on
+xlim([1000 11000])
+ax = gca; 
+ax.XAxis.FontSize = 16;
+ax.YAxis.FontSize = 16;
+ax.ZAxis.FontSize = 16;
+set(gca,'LooseInset',get(gca,'TightInset'));
+title('mean flow time vs. probability of excursion, different k slices','FontSize',16)
+
+hold off
